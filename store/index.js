d708ad32efb37d96f763ae37fba6cdd187567f37
Paul <spread2009@gmail.com>
2017-08-08 23:07:12 +0300

    vue table

---
+++ store/index.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+  todos: [
+    { task: 'eat' },
+    { task: 'sleep' },
+    { task: 'code' }
+  ]
+  // counter: 0
+})
+
+export const mutations = {
+
+  //   increment (state) {
+  //     state.counter++
+  //   },
+  //   decrement (state) {
+  //     state.counter--
+  //   }
+}
